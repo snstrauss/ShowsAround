cc3aa27a37af3786e16c704fdde0e70ac217d9f4
Guy Zahavi <guyz@onoapps.com>
2017-04-05 18:50:53 +0300

    whatever

--- .vscode/launchReactNative.js
+++ .vscode/launchReactNative.js
@@ -3,7 +3,7 @@
 try {
     var path = require("path");
     var Launcher = require("/Users/guyzahavi/.vscode/extensions/vsmobile.vscode-react-native-0.2.5/out/debugger/launcher.js").Launcher;
-    new Launcher("/Users/guyzahavi/myStuff/myWork/ShowsAround", "/Users/guyzahavi/myStuff/myWork/ShowsAround").launch();
+    new Launcher("/Users/guyzahavi/Sites/ShowsAround", "/Users/guyzahavi/Sites/ShowsAround").launch();
 } catch (e) {
     throw new Error("Unable to launch application. Try deleting .vscode/launchReactNative.js and restarting vscode.");
 }
